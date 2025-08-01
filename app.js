e96164a51b4d6e03019acf497a176d7cbe468e5d
chz42 <chriz.40ribeiro@gmail.com>
2025-08-01 01:07:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // app.js
 
-// Importante: Substitua com as credenciais do seu projeto Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyARk0YjJJUkCtPwK0GX2VykbZI0MfSK3OU",
+  authDomain: "familia-ribeiro-f692a.firebaseapp.com",
+  projectId: "familia-ribeiro-f692a",
+  storageBucket: "familia-ribeiro-f692a.firebasestorage.app",
+  messagingSenderId: "36243339331",
+  appId: "1:36243339331:web:7a35ba08968469bd7ebcf6"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
